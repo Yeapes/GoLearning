280906edf0639768363c8b02219463878967180c
Yeapes <yeapesh2831@gmail.com>
2023-01-09 00:08:08 +0600

    Type Casting

---
+++ type_casting/main.go
@@ -0,0 +1,15 @@
+package main
+
+import "fmt"
+
+func main(){
+	var age int = 10
+	var fage float64 = float64(age)
+	fmt.Printf("Age: %f \n",fage)
+	
+	//Float to integer
+
+	var experience float64 = 2.5
+	var experience_int int = int(experience)
+	fmt.Printf("%v",experience_int)
+}
