4c9ea0a2eafc42682aaf156bb74fed10ff7cb9bf
Yeapes <yeapesh2831@gmail.com>
2023-01-16 00:11:56 +0600

    control flow

---
+++ control_flow/main.go
@@ -0,0 +1,15 @@
+package main
+
+import "fmt"
+
+func main(){
+	age := 23;
+
+	if age >= 18{
+		fmt.Println("You have right access to the website")
+	}else if age <= 10{
+		fmt.Println("You need to parental access")
+	}else {
+		fmt.Println("It's not a correct value to check condition")
+	}
+}
