d15e26d43d0a815e6f0fca4439414ae89b0c693a
Yeapes <yeapesh2831@gmail.com>
2023-01-16 23:30:59 +0600

    Array added

---
+++ arrray/main.go
@@ -0,0 +1,17 @@
+package main
+import "fmt"
+
+func main(){
+	var arr1 = [3]int{11,2,3}
+	arr2 :=[2]string{"ye"}
+	fmt.Println(arr1)
+	fmt.Println(arr2)
+
+	arr3 := [5]int{1:10,2:40}
+
+  	fmt.Println(arr3)
+
+
+	  fmt.Println(len(arr1))
+	  fmt.Println(len(arr2))
+}
