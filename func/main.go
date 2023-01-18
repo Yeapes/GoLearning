6438fce82205ede768abd558034b6f28adb1c15e
Yeapes <yeapesh2831@gmail.com>
2023-01-19 00:08:45 +0600

    func added

--- func/main.go
+++ func/main.go
@@ -7,9 +7,14 @@ func myFunction(x int, y string) (result int, txt1 string) {
   return
 }
 
+func myFunction2(x int, y int) int {
+  return x + y
+}
+
 func main() {
 
 	//we can omit using _
   a, b := myFunction(5, "Hello")
   fmt.Println(a, b)
+fmt.Println(myFunction2(1,2));
 }
